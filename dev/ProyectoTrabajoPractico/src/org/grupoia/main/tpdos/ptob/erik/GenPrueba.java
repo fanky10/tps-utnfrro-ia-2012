ccd4085aa8be4a04838110fa100c21751b5c3901
erika.s.gustafsson <erika.s.gustafsson@MW7Y93Z7IVK2QE.dir.svc.accenture.com>
2012-08-09 18:20:36 -0500

    MyGeneracion y prueba by erik

---
+++ dev/ProyectoTrabajoPractico/src/org/grupoia/main/tpdos/ptob/erik/GenPrueba.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.grupoia.main.tpdos.ptob.erik;
+
+/**
+ *
+ * @author erika.s.gustafsson
+ */
+public class GenPrueba {
+    
+}
